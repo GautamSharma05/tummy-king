0276040ae306dda607e7a17d7684e11696411fa4
Gautam Sharma <gautam@zibtek.in>
2024-05-09 20:48:54 +0530

    chore: Update About Us page with content and styling

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function AboutUs() {
   return (
-    <div>page</div>
+    <section className='container'>
+    <h2 className='text-3xl font-semibold my-5'>About Us</h2>
+    <p>
+      <span className='text-lg'>Welcome to Tummy King, your number one source for all tasty recipes. We&apos;re dedicated to providing you with the best recipes, with an emphasis on taste, quality, and easy-to-make recipes.</span>
+    </p>
+    <br/>
+    <p>
+      <span  className='text-lg'>Founded by LOKESH SHARMA, tummyking has come a long way from its beginnings in BEAWAR, (AJMER). When LOKESH SHARMA first started out, his passion for cooking the best taste for his foodies drove him to start his own business.</span>
+    </p>
+    <br/>
+    <p>
+      <span  className='text-lg'>We hope you enjoy our recipes as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us.</span>
+    </p>
+    </section>
   )
 }
--- src/components/ContactUsForm/ContactUsForm.tsx
+++ src/components/ContactUsForm/ContactUsForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
   FormControl,
@@ -26,10 +26,9 @@ const formSchema = z.object({
   subject: z.string().min(5, {
     message: "Subject must be at least 5 characters.",
   }),
-    message: z.string().min(10, {
-        message: "Message must be at least 10 characters.",
-    }),
-
+  message: z.string().min(10, {
+    message: "Message must be at least 10 characters.",
+  }),
 });
 export default function ContactUsForm() {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -46,66 +45,74 @@ export default function ContactUsForm() {
     console.log(values);
   }
   return (
-    <div className="border-2 w-[300px]  md:w-[450px] border-black p-5">
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="fullName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Full Name</FormLabel>
-              <FormControl>
-                <Input placeholder="enter your full name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="enter your email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="subject"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Subject</FormLabel>
-              <FormControl>
-                <Input placeholder="enter your subject" {...field} />
-              </FormControl>
-           
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="message"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Your Message</FormLabel>
-              <FormControl>
-                <Textarea placeholder="enter your message" {...field} />
-              </FormControl>
-              <FormDescription>Your message will be handle by support team.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button className="w-full" type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="border-2 w-[350px]  md:w-[450px] border-black p-5">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="enter your full name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Your Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="enter your email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="subject"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Subject</FormLabel>
+                <FormControl>
+                  <Input placeholder="enter your subject" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Your Message</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="enter your message" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Your message will be handle by support team.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
